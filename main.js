b488f61072ac85b17672ad3ebe30a127b383c4cc
Alina Sharafutdinova <49404655+Shaaleena@users.noreply.github.com>
2020-05-08 13:43:18 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 function myFunction() {
   var x = document.getElementById("myLinks");
   var y= document.getElementById("hero");
-
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    y.style.marginTop="15%";
-
+  if (x.className === "topnav" && y.className==="hero-img12-res") {
+    x.className += " responsive";
   } else {
-    x.style.display = "block";
-    y.style.marginTop="0";
-
+    x.className = "topnav";
   }
-}
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
