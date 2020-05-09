699b2c42f75ad7f4ed2f3e57d3a05c853cebc40e
Alina Sharafutdinova <49404655+Shaaleena@users.noreply.github.com>
2020-05-09 12:37:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,28 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal
+window.onload = function() {
+  setTimeout(function(){
+  modal.style.display = "block";
+}, 2000);
+  modal.className +=" fade-in";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
