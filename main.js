ba437c57299b9d6ce25b3b2c6771002b4e90b6db
Alina Sharafutdinova <49404655+Shaaleena@users.noreply.github.com>
2020-07-03 13:15:11 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,55 @@
+
 function myFunction() {
   var x = document.getElementById("myLinks");
   var y= document.getElementById("hero");
   if (x.className === "topnav") {
     x.className += " responsive";
+
   } else {
     x.className = "topnav";
+
   }
 }
-var modal = document.getElementById("myModal");
+
+  var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
+
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+
 // When the user clicks the button, open the modal
+
 window.onload = function() {
   setTimeout(function(){
   modal.style.display = "block";
 }, 2000);
   modal.className +=" fade-in";
 }
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
+//  if (x.style.display === "block") {
+//    x.style.display = "none";
+//    y.style.marginTop="15%";
+
+//  } else {
+//    x.style.display = "block";
+//    y.style.marginTop="0";
+
+//  }
+
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
